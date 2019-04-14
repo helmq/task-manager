fa6b226821c764b4c906a9e61ce0009d031bd4b1
Viktor <jarlaks52@gmail.com>
2019-04-14 11:50:05 +0900

    add coveralls and simplecov, add coveralls badge, fix travis config

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Task Manager
 [![Build Status](https://travis-ci.org/helmq/task-manager.svg?branch=feature%2Fdevops)](https://travis-ci.org/helmq/task-manager)
+[![Coverage Status](https://coveralls.io/repos/github/helmq/task-manager/badge.svg?branch=feature%2Fcoveralls)](https://coveralls.io/github/helmq/task-manager?branch=feature%2Fcoveralls)
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,11 @@
+require 'simplecov'
+require 'coveralls'
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start 'rails'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  # fixtures :all
-
-  # Add more helper methods to be used by all tests here...
   include FactoryBot::Syntax::Methods
 end
