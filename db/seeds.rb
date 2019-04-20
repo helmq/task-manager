407c88894adee0e348cfcd0ec87018622f4280ab
Viktor <jarlaks52@gmail.com>
2019-04-20 22:35:00 +0900

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,10 +3,7 @@ admin.password = 'admin'
 admin.save
 
 60.times do |i|
-  u = [Manager, Developer].sample.new
-  u.email = "email#{i}@mail.gen"
-  u.first_name = "FN#{i}"
-  u.last_name = "LN#{i}"
+  u = [Manager, Developer].sample.find_or_create_by(email: "email#{i}@mail.gen", first_name: "FN#{i}", last_name: "LN#{i}")
   u.password = i.to_s
   u.save
 end
