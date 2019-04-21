901cc2b7985e73590e0ff135017ba019ce3694df
Viktor <jarlaks52@gmail.com>
2019-04-21 15:33:13 +0900

    add task serializer; update api::v1::tasks_controller

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -15,4 +15,14 @@ class Api::V1::TasksController < Api::V1::ApplicationController
 
     respond_with json
   end
+
+  def build_meta_tasks(collection)
+    {
+      count: collection.count,
+      total_count: collection.total_count,
+      current_page: collection.current_page,
+      total_pages: collection.total_pages,
+      per_page: collection.limit_value
+    }
+  end
 end
