f7d7ffa1f29657f10a5d14753c117074810bdd71
Viktor <jarlaks52@gmail.com>
2019-04-22 19:40:05 +0900

    define show, create, update, destroy, private:task_params; remove build_meta_tasks

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::TasksController < Api::V1::ApplicationController
+  respond_to :json
+
   def index
     q_params = params[:q] || { s: 'id asc' }
 
@@ -16,13 +18,43 @@ class Api::V1::TasksController < Api::V1::ApplicationController
     respond_with json
   end
 
-  def build_meta_tasks(collection)
-    {
-      count: collection.count,
-      total_count: collection.total_count,
-      current_page: collection.current_page,
-      total_pages: collection.total_pages,
-      per_page: collection.limit_value
-    }
+  def show
+    task = Task.find(params[:id])
+    respond_with task
+  end
+
+  def create
+    task = current_user.my_tasks.new(task_params)
+
+    if task.save
+      respond_with(task, location: nil)
+    else
+      render(json: { errors: task.errors }, status: :unprocessable_entity)
+    end
+  end
+
+  def update
+    task = Task.find(params[:id])
+
+    if task.update(task_params)
+      render(json: task)
+    else
+      render(json: { errors: task.errors }, status: :unprocessable_entity)
+    end
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    if task.destroy
+      head(:ok)
+    else
+      render(json: { errors: task.errors }, status: :unprocessable_entity)
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description, :author_id, :assignee_id, :state_event)
   end
 end
