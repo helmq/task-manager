6ce537421fa8194c8de488f823fe9cb773552d87
Viktor <jarlaks52@gmail.com>
2019-04-25 13:32:21 +0900

    fix seeds (find or create only by email)

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,9 @@ admin.password = 'admin'
 admin.save
 
 60.times do |i|
-  u = [Manager, Developer].sample.find_or_create_by(email: "email#{i}@mail.gen", first_name: "FN#{i}", last_name: "LN#{i}")
+  u = [Manager, Developer].sample.find_or_create_by(email: "email#{i}@mail.gen")
+  u.first_name = "FN#{i}"
+  u.last_name = "LN#{i}"
   u.password = i.to_s
   u.save
 end
